e74e7c326e1a352e4c1802752af656b92fcb42bb
squart <squart300kg@naver.com>
2022-12-18 19:34:18 +0900

    이스트소프트 코딩테스트 대비 Set활용

--- app/src/main/java/com/example/starter/ui/firstTab/FirstTabFragment.kt
+++ app/src/main/java/com/example/starter/ui/firstTab/FirstTabFragment.kt
@@ -7,7 +7,42 @@ import androidx.fragment.app.viewModels
 import com.example.starter.R
 import com.example.starter.base.BaseFragment
 import com.example.starter.databinding.FirstTabFragmentBinding
+import kotlinx.coroutines.*
+import java.util.*
 
+class Solution {
+
+    private val MIN_COORDINATE_VALUE = 1
+    private val MAX_COORDINATE_VALUE = 1000000000
+
+    private val INPUT_COORDINATE_COUNT = 3
+
+    private val X = 0
+    private val Y = 1
+
+    fun solution(tripleCoordinate: Array<IntArray>): IntArray {
+        require(tripleCoordinate.size == INPUT_COORDINATE_COUNT)
+
+        val noDulplicatedXCoordinateSet = mutableSetOf<Int>()
+        val noDulplicatedYCoordinateSet = mutableSetOf<Int>()
+
+
+        tripleCoordinate.forEachIndexed { index, coordinate ->
+            require(coordinate.all { it in MIN_COORDINATE_VALUE..MAX_COORDINATE_VALUE })
+
+            if (!noDulplicatedXCoordinateSet.add(coordinate[X])) {
+                noDulplicatedXCoordinateSet.remove(coordinate[X])
+            }
+            if (!noDulplicatedYCoordinateSet.add(coordinate[Y])) {
+                noDulplicatedYCoordinateSet.remove(coordinate[Y])
+            }
+
+        }
+
+        return noDulplicatedXCoordinateSet.toIntArray() + noDulplicatedYCoordinateSet.toIntArray()
+    }
+
+}
 class FirstTabFragment : BaseFragment<FirstTabFragmentBinding>(R.layout.first_tab_fragment) {
 
     private val firstTabViewModel: FirstTabViewModel by viewModels()
@@ -16,6 +51,9 @@ class FirstTabFragment : BaseFragment<FirstTabFragmentBinding>(R.layout.first_ta
         super.onViewCreated(view, savedInstanceState)
         Log.i("bottomId", "First Loaded")
 
+        val result = Solution().solution(arrayOf(intArrayOf(1,4), intArrayOf(3,4), intArrayOf(3,10)))
+        binding.tvTitle.text = "${result[0]}, ${result[1]}"
+        Log.i("result : " ,"$result")
     }
 
 }
