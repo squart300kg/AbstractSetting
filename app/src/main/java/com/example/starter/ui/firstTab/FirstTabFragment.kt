70e1f68cf44368ef5dd5e3f2a42f0f4c243f95f5
squart <squart300kg@naver.com>
2022-12-19 19:55:15 +0900

    이스트소프트 코딩테스트 대비 [타겟넘버]

--- app/src/main/java/com/example/starter/ui/firstTab/FirstTabFragment.kt
+++ app/src/main/java/com/example/starter/ui/firstTab/FirstTabFragment.kt
@@ -7,8 +7,58 @@ import androidx.fragment.app.viewModels
 import com.example.starter.R
 import com.example.starter.base.BaseFragment
 import com.example.starter.databinding.FirstTabFragmentBinding
-import kotlinx.coroutines.*
-import java.util.*
+
+class Solution2 {
+
+    val MIN_INPUT_NUMBER_COUNT = 2
+    val MAX_INPUT_NUMBER_COUNT = 20
+
+    val MIN_INPUT_NUMBER = 1
+    val MAX_INPUT_NUMBER = 50
+
+    val MIN_TARGET_NUMBER = 1
+    val MAX_TARGET_NUMBER = 1000
+
+    var matchedTargetCount = 0
+
+    private fun checkArgsValidation(numbers: IntArray, target: Int) {
+        require(numbers.size in MIN_INPUT_NUMBER_COUNT..MAX_INPUT_NUMBER_COUNT)
+        numbers.forEach { number ->
+            require(number in MIN_INPUT_NUMBER..MAX_INPUT_NUMBER)
+        }
+        require(target in MIN_TARGET_NUMBER..MAX_TARGET_NUMBER)
+    }
+
+    fun solution(numbers: IntArray, target: Int): Int {
+
+        checkArgsValidation(numbers, target)
+
+        getResultFromDSF(numbers = numbers, target = target)
+
+        return matchedTargetCount
+    }
+
+
+    private fun getResultFromDSF(numbers: IntArray,
+                                 target: Int,
+                                 incompleteTarget: Int = 0,
+                                 depthIndex: Int = 0
+    ) {
+        Log.i("DSF : " ,"depthIndex : $depthIndex")
+        Log.i("DSF : " ,"incompleteTarget : $incompleteTarget")
+
+        if (depthIndex == numbers.size) {
+            if (incompleteTarget == target) {
+                matchedTargetCount++
+                Log.i("DSF : " ,"matchedTargetCount : $matchedTargetCount")
+            }
+            return
+        }
+
+        getResultFromDSF(numbers, target, incompleteTarget + numbers[depthIndex], depthIndex + 1)
+        getResultFromDSF(numbers, target, incompleteTarget - numbers[depthIndex], depthIndex + 1)
+    }
+}
 
 class Solution {
 
@@ -63,9 +113,10 @@ class FirstTabFragment : BaseFragment<FirstTabFragmentBinding>(R.layout.first_ta
 
 //        val result = Solution().solution(arrayOf(intArrayOf(1,4), intArrayOf(3,4), intArrayOf(3,10)))
 //        binding.tvTitle.text = "${result[0]}, ${result[1]}"
-        val result = Solution().solution2(12)
-        binding.tvTitle.text = "${result}"
-        Log.i("result : " ,"$result")
+//        val result = Solution().solution2(12)
+        val result = Solution2().solution(intArrayOf(1,1,1,1,1),3)
+        Log.i("DSF : " ,"$result")
+//        binding.tvTitle.text = "${result}"
     }
 
 }
